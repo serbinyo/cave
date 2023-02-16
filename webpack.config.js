ef685220e9a9bd8189b87c01bf33adc523e5d95d
Сербин Александр <serbin@rarus.ru>
2023-02-16 21:08:10 +0300

    Настроен webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('wim', './assets/js/wim.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -71,6 +72,7 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    .enableVueLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
