4bff611caf0291ef4001eeaab7b5235bb15f8e7a
Сербин Александр <serbin@rarus.ru>
2023-02-19 18:56:50 +0300

    Добавление индексной страницы

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Service\Serializer\Deserializer;
-use App\Service\Serializer\Wim\BreathingExerciseDeserializer;
-use App\UseCase\Wim\FillUserExercises\Command;
-use App\UseCase\Wim\FillUserExercises;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -28,33 +25,12 @@ class IndexController extends AbstractController
     }
 
     /**
-     *
-     * @throws \Doctrine\DBAL\Exception
-     * @throws \Exception
-     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @return Response
      */
     #[Route('/', name: 'homepage')]
-    public function index(FillUserExercises\Handler $getExercisesService, Deserializer $deserializer)
+    public function index()
     {
-        $user = $this->getUser();
-        $userExercises = [];
-
-        if ($user !== null) {
-            $getExercisesService->handle(
-                new Command($user)
-            );
-
-            $userExercises = BreathingExerciseDeserializer::deserializeCollection(
-                $user->getBreathingExercises(),
-                'array'
-            );
-        }
-
-        $response =  $this->render('index/index.html.twig',
-            [
-                'userExercises' => $userExercises
-            ]
-        );
+        $response =  $this->render('homepage/index.html.twig');
 
         return $response;
     }
--- src/Controller/WimMainController.php
+++ src/Controller/WimMainController.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\Serializer\Deserializer;
+use App\Service\Serializer\Wim\BreathingExerciseDeserializer;
+use App\UseCase\Wim\FillUserExercises\Command;
+use App\UseCase\Wim\FillUserExercises;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -23,15 +27,37 @@ class WimMainController extends AbstractController
         $this->twig = $twig;
     }
 
+
     /**
+     * @param FillUserExercises\Handler $getExercisesService
+     * @param Deserializer              $deserializer
      *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\DBAL\Exception
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
      */
     #[Route('/wim', name: 'wim')]
-    public function index()
+    public function index(FillUserExercises\Handler $getExercisesService, Deserializer $deserializer)
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        $userExercises = [];
 
-        $response =  $this->render('wim/index.html.twig');
+        if ($user !== null) {
+            $getExercisesService->handle(
+                new Command($user)
+            );
+
+            $userExercises = BreathingExerciseDeserializer::deserializeCollection(
+                $user->getBreathingExercises(),
+                'array'
+            );
+        }
+
+        $response =  $this->render('wim/index.html.twig',
+            [
+                'userExercises' => $userExercises
+            ]
+        );
 
         return $response;
     }
